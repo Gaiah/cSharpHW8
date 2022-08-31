3d966ff8871682d99cbc1d31567064ec3431d0de
Gaiah <gaiah.a.akopian@gmail.com>
2022-09-01 03:07:55 +0400

    Task_56 added

---
+++ Task_56/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void NewRandomMatrix (int[,] matr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(1, 20);
+            System.Console.Write($"{matr[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+void FindMinSumString (int[,] matrix1)
+{   
+    int sum = 0;   
+    int minSum = 2147483647; //максимальное возможное значение инт
+    int minString = 0;
+
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            sum += matrix1[i, j];            
+        }
+        
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minString = i + 1;
+        }
+        sum = 0;
+    }
+    System.Console.WriteLine($"The lowest element sum string is the string #{minString} and its sum = {minSum} ");     
+}
+
+
+int[,] matrix = new int[6,3];
+NewRandomMatrix(matrix);
+FindMinSumString(matrix);
