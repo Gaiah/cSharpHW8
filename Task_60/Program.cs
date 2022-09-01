53fe94e469fa8bceeeab430655c5d88a9c286424
Gaiah <gaiah.a.akopian@gmail.com>
2022-09-01 19:40:16 +0400

    Task_60 added

---
+++ Task_60/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+
+void ThreeDimensArray (int[,,] cube)
+{
+    int num = 10;
+    for (int k = 0; k < cube.GetLength(0); k++)
+    {
+        num += 5;
+        for (int j = 0; j < cube.GetLength(1); j++)
+        {
+            num += 3;
+            for (int i = 0; i < cube.GetLength(2); i++)
+            {   
+                num += 9;
+                cube[k, i, j] = num;     
+                System.Console.Write($"{k}, {i}, {j} - {cube[k, i, j]}\t");                 
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
+
+int[,,] array = new int[2, 2, 2];
+ThreeDimensArray(array);
